94246f94544ab90dfa263ea33ea5c72ce8307a1e
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-09-18 21:12:28 +0900

    ブログ作成機能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,18 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: 'ブログを作成しました！'
+    else
+      render :new
+    end
+  end
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
