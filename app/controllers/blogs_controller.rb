3d40d369cf055a39b8a9c937b61e8cb6360b403d
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2016-10-02 05:59:38 +0000

    #19 修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,3 @@
 class BlogsController < ApplicationController
+    before_action :authenticate_user! 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,2 @@
 class Blog < ActiveRecord::Base
-   before_action :authenticate_user! 
 end
