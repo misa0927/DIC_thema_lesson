87f6bf3b78f2b3d87d8c0a9745fb0d08760440f9
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2016-10-02 05:42:55 +0000

    調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_many :blogs, :dependent => destroy
+  has_many :blogs, :dependent => :destroy
 
 end
