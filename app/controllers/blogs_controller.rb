6d89ae4856e069ba1e52af091d5bced73e890910
宮田裕貴 <miyatayuki@miyatayuki-no-MacBook-Pro.local>
2016-10-02 15:04:03 +0900

    action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,26 @@
 class BlogsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   private
     def blog_params
       params.require(:blog).permit(:title, :content)
